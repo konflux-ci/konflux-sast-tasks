a74b921f673b4b580caddbfe0da94215458e995c
Sam Fowler <sfowler@redhat.com>
2025-11-12 12:56:13 +1000

    fixup

--- task/prep-rpm-source/0.1/tests/pre-apply-task-hook.sh
+++ task/prep-rpm-source/0.1/tests/pre-apply-task-hook.sh
@@ -5,12 +5,14 @@
 # PUSH_USERNAME="${TEST_PREP_RPM_PUSH_USERNAME}"
 PUSH_SECRET="${TEST_PREP_RPM_PUSH_DOCKERCONFIG}" # should be base64 encoded
 PUSH_SECRET_NAME=prep-rpm-source-push-secret
-SERVICE_ACCOUNT_NAME=default
+SERVICE_ACCOUNT_NAME=appstudio-pipeline
 echo "Creating image push secret $PUSH_SECRET_NAME in namespace: $2"
 
 kubectl get sa
 
-echo $PUSH_SECRET | base64 -d > /tmp/dockerconfig.json
+kubect -n "$2" create serviceaccount "$SERVICE_ACCOUNT_NAME" || true
+
+echo -n "$PUSH_SECRET" | base64 -d > /tmp/dockerconfig.json
 kubectl -n "$2" create secret docker-registry "$PUSH_SECRET_NAME" --from-file=/tmp/dockerconfig.json
 # kubectl -n "$2" create secret docker-registry "$PUSH_SECRET_NAME" --docker-username "$PUSH_USERNAME" --docker-password "$PUSH_PASSWORD"
 
