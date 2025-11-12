e3d5745da49b8cdfde81d7b482f1f8f6c15d5f5e
Sam Fowler <sfowler@redhat.com>
2025-11-12 12:17:52 +1000

    fixup

--- task/prep-rpm-source/0.1/tests/pre-apply-task-hook.sh
+++ task/prep-rpm-source/0.1/tests/pre-apply-task-hook.sh
@@ -5,8 +5,9 @@ PUSH_PASSWORD="${TEST_PREP_RPM_PUSH_PASSWORD}"
 PUSH_USERNAME="${TEST_PREP_RPM_PUSH_USERNAME}"
 PUSH_SECRET_NAME=prep-rpm-source-push-secret
 SERVICE_ACCOUNT_NAME=appstudio-pipeline
-echo "Creating image push secret in namespace: $2"
+echo "Creating image push secret $PUSH_SECRET_NAME in namespace: $2"
 
-kubectl --namespace "$2" create secret docker-registry "$PUSH_SECRET_NAME" \
-  --docker-username "$PUSH_USERNAME" --docker-password "$PUSH_PASSWORD"
-kubectl --namespace "$2" patch serviceaccount "$SERVICE_ACCOUNT_NAME" -p '{"imagePullSecrets": [{"name": "'"$MY_SECRET_NAME"'"}]}'
+oc get sa
+
+kubectl -n "$2" create secret docker-registry "$PUSH_SECRET_NAME" --docker-username "$PUSH_USERNAME" --docker-password "$PUSH_PASSWORD"
+kubectl -n "$2" patch serviceaccount "$SERVICE_ACCOUNT_NAME" -p '{"imagePullSecrets": [{"name": "'"$MY_SECRET_NAME"'"}]}'
