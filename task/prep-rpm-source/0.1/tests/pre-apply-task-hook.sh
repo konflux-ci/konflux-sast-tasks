3fe5416f501cbca3089d1b4d3986f5e8b3dd63ea
Sam Fowler <sfowler@redhat.com>
2025-11-12 13:34:37 +1000

    fixup

--- task/prep-rpm-source/0.1/tests/pre-apply-task-hook.sh
+++ task/prep-rpm-source/0.1/tests/pre-apply-task-hook.sh
@@ -6,20 +6,17 @@ if [[ -z "${TEST_PREP_RPM_PUSH_DOCKERCONFIG:-}" ]]; then
     exit 1
 fi
 
-# these should exist as github actions secrets
-# PUSH_PASSWORD="${TEST_PREP_RPM_PUSH_PASSWORD}"
-# PUSH_USERNAME="${TEST_PREP_RPM_PUSH_USERNAME}"
-PUSH_SECRET="${TEST_PREP_RPM_PUSH_DOCKERCONFIG}" # should be base64 encoded
+# should exist as github actions secret, base64 encoded
+PUSH_SECRET="${TEST_PREP_RPM_PUSH_DOCKERCONFIG}"
 PUSH_SECRET_NAME=prep-rpm-source-push-secret
 SERVICE_ACCOUNT_NAME=appstudio-pipeline
 echo "Creating image push secret $PUSH_SECRET_NAME in namespace: $2"
 
-kubectl get sa
-
+# appstudio-pipeline will be created later by run-task-tests.sh script
+# but we need it now so we can link it to the push secret
 kubectl -n "$2" create serviceaccount "$SERVICE_ACCOUNT_NAME" || true
 
 echo -n "$PUSH_SECRET" | base64 -d > /tmp/dockerconfig.json
 kubectl -n "$2" create secret docker-registry "$PUSH_SECRET_NAME" --from-file=/tmp/dockerconfig.json
-# kubectl -n "$2" create secret docker-registry "$PUSH_SECRET_NAME" --docker-username "$PUSH_USERNAME" --docker-password "$PUSH_PASSWORD"
 
 kubectl -n "$2" patch serviceaccount "$SERVICE_ACCOUNT_NAME" -p '{"imagePullSecrets": [{"name": "'"$PUSH_SECRET_NAME"'"}]}'
