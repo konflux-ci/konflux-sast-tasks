4cb31b9537d6550a660e34b46c6a123a77a9669c
Sam Fowler <sfowler@redhat.com>
2025-11-12 14:35:38 +1000

    snyk: check token is present for test

--- task/sast-snyk-check/0.4/tests/pre-apply-task-hook.sh
+++ task/sast-snyk-check/0.4/tests/pre-apply-task-hook.sh
@@ -4,9 +4,13 @@ echo "Removing computeResources for task: $1"
 yq -i eval '.spec.steps[0].computeResources = {}' $1
 yq -i eval '.spec.steps[1].computeResources = {}' $1
 
+if [ -z "${TEST_SNYK_SECRET}" ]; then
+  echo "TEST_SNYK_SECRET env variable not defined"
+  exit 1
+fi
+
 # Create snyk secret with value from TEST_SNYK_SECRET environment variable
-# Default to "fake-token" if not set
-SNYK_TOKEN="${TEST_SNYK_SECRET:-fake-token}"
+SNYK_TOKEN="${TEST_SNYK_SECRET}"
 echo "Creating snyk secret in namespace: $2"
 kubectl create secret generic snyk-secret \
     --from-literal=snyk_token="$SNYK_TOKEN" \
