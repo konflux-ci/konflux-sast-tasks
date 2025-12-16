5929a3967f35ddc0c056f867502a41739583407b
Sam Fowler <sfowler@redhat.com>
2025-12-16 12:28:05 +1000

    ci: use TEST_SNYK_TOKEN secret

--- task/sast-snyk-check/0.4/tests/pre-apply-task-hook.sh
+++ task/sast-snyk-check/0.4/tests/pre-apply-task-hook.sh
@@ -4,13 +4,13 @@ echo "Removing computeResources for task: $1"
 yq -i eval '.spec.steps[0].computeResources = {}' $1
 yq -i eval '.spec.steps[1].computeResources = {}' $1
 
-if [ -z "${TEST_SNYK_SECRET}" ]; then
-  echo "TEST_SNYK_SECRET env variable not defined"
+if [ -z "${TEST_SNYK_TOKEN}" ]; then
+  echo "TEST_SNYK_TOKEN env variable not defined"
   exit 1
 fi
 
-# Create snyk secret with value from TEST_SNYK_SECRET environment variable
-SNYK_TOKEN="${TEST_SNYK_SECRET}"
+# Create snyk secret with value from TEST_SNYK_TOKEN environment variable
+SNYK_TOKEN="${TEST_SNYK_TOKEN}"
 echo "Creating snyk secret in namespace: $2"
 kubectl create secret generic snyk-secret \
     --from-literal=snyk_token="$SNYK_TOKEN" \
