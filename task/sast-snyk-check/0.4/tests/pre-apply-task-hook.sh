ab88639100e4de9565c4d7370a322a68c49fd740
Kasem Alem <kalem@redhat.com>
2026-02-09 13:55:10 +0200

    fix(KFLUXSPRT-5245): fix task sast-snyk-check finds no sarif files
    Signed-off-by: Kasem Alem <kalem@redhat.com>

--- task/sast-snyk-check/0.4/tests/pre-apply-task-hook.sh
+++ task/sast-snyk-check/0.4/tests/pre-apply-task-hook.sh
@@ -4,14 +4,12 @@ echo "Removing computeResources for task: $1"
 yq -i eval '.spec.steps[0].computeResources = {}' $1
 yq -i eval '.spec.steps[1].computeResources = {}' $1
 
-if [ -z "${TEST_SNYK_TOKEN}" ]; then
-  echo "TEST_SNYK_TOKEN env variable not defined"
-  exit 1
+if [ -n "${TEST_SNYK_TOKEN}" ]; then
+  # Create snyk secret with value from TEST_SNYK_TOKEN environment variable
+  echo "Creating snyk secret in namespace: $2"
+  kubectl create secret generic snyk-secret \
+      --from-literal=snyk_token="${TEST_SNYK_TOKEN}" \
+      --namespace="$2" || true
+else
+  echo "TEST_SNYK_TOKEN not set; task will run in skip mode (no secret created)"
 fi
-
-# Create snyk secret with value from TEST_SNYK_TOKEN environment variable
-SNYK_TOKEN="${TEST_SNYK_TOKEN}"
-echo "Creating snyk secret in namespace: $2"
-kubectl create secret generic snyk-secret \
-    --from-literal=snyk_token="$SNYK_TOKEN" \
-    --namespace="$2" || true
